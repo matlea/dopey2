1536a2ae2566e65e6a70dffe1b1bed2b11edea84
matlea <mats.leandersson@maxiv.lu.se>
2026-02-10 17:28:23 +0100

    _DataObject() was, for stupid reasons, moved out of dopey_loader.py and into its own file, dopey_data_object.py

---
+++ dopey/dopey_data_object.py
@@ -0,0 +1,131 @@
+__version__ = "26.02.10"
+__author__  = "Mats Leandersson"
+
+"""
+version 26.02.10    Moving the _DataObject out of dopey_loader.py due to various reasons.
+                    Renaming it DataObject.
+"""
+
+
+import numpy as np
+from colorama import Fore
+
+
+
+class DataObject():
+    """
+    This is an object that contains loaded, sorted data, and perhaps manipulated data from Prodigy,
+    e.g. created by load(), asymmetry(), etc.
+    The data and metadata is accessible as attributes, e.g. .intensity, .axis0, etc.
+    A holds a couple of methods, e.g. .plot().
+    
+    """
+    def _addProperty(self, name: str, value):
+        setattr(DataObject, name, property(self._getter(name), self._setter(name, value)))
+        setattr(self, '__'+name, value)
+    
+    def _setter(self, name: str, value):
+        def inner_setter(self, value):
+            setattr(self, '__'+name, value)
+        return inner_setter
+
+    def _getter(self, name: str):
+        def inner_getter(self):
+            return getattr(self, '__'+name)
+        return inner_getter
+    
+    def __init__(self, **kwargs):
+        """
+        """    
+        self._addProperty("dopey", __version__)
+    
+    def __str__(self):
+        return dataInfo(self, ret = True)
+    
+    @property
+    def info(self):
+        dataInfo(self)
+    
+    @property
+    def arrays(self):
+        lst = self._listArrays()
+        print(f"Arrays:")
+        for attr in lst:
+            shape = np.shape(self.__dict__["__"+attr])
+            print(f"  {Fore.BLUE}{attr.ljust(20)}{Fore.RESET}shape={shape}")
+            
+    
+    # --------------------------------------------------------------------------
+    
+    def _printDataType(self):
+        print(f"Data type:\n  {Fore.BLUE}{self.data_type}{Fore.RESET}")
+        
+    def _printAxesAndParameters(self):
+        print(f"Data, axes, and parameters:")
+        for key in self.__dict__.keys():
+            name, value = "", ""
+            if "label" in key:
+                name = key.replace("_label", "").replace("__", "")
+                shape = np.shape(self.__dict__["__"+name])
+                print(f"  {Fore.BLUE}{name.ljust(20)}{Fore.RESET}{self.__dict__[key].ljust(25)}shape={shape}")
+    
+    def _listAttributes(self):
+        lst = []
+        for key in self.__dict__.keys(): lst.append(key.replace("__",""))
+        return lst
+    
+    def _listArrays(self):
+        lst = []
+        for key in self.__dict__.keys(): 
+            if type(self.__dict__.get(key, None)) is np.ndarray:
+                lst.append(key.replace("__",""))
+        return lst
+        
+
+
+
+def dataInfo(D = None, ret = False):
+    try: _ = D.dopey
+    except:
+        print(f"{Fore.RED}info(): the argument must be a Data object."); return
+    #
+    out = ""
+    for key in D.__dict__.keys():
+        if key.startswith("__"):
+            key_name = Fore.BLUE + key.replace('__', '').ljust(20) + Fore.RESET
+            item = D.__dict__.get(key)
+            typ = type(item)
+            if typ is np.ndarray:
+                t = "Array".ljust(15)
+                s = f"shape = {np.shape(item)}".ljust(25)
+                nk = f"{key}_label"
+                out += f"{key_name}{t}{s}{D.__dict__.get(nk,'')}\n"
+            #elif typ is str and not "_label" in key_name:
+            #    t = "String".ljust(15)
+            #    s = item.replace("\n", " ").replace("\t", " ")
+            #    print(f"{key_name}{t}{s}")
+            elif typ is float or typ is int or typ is np.float64:
+                t = "Scalar".ljust(15)
+                v = item
+                out += f"{key_name}{t}{v}\n"
+    out += "-\n"
+    #
+    for key in D.__dict__.keys():
+        if key.startswith("__"):
+            key_name = Fore.BLUE + key.replace('__', '').ljust(20) + Fore.RESET
+            item = D.__dict__.get(key)
+            typ = type(item)
+            if typ is dict:
+                t = "Dictionary".ljust(15)
+                out += f"{key_name}{t}\n"
+            elif typ is str:# and "_label" in key_name:
+                if not "dopey" in key_name:
+                    t = "String".ljust(15)
+                    s = item.replace("\n", " ").replace("\t", " ")
+                    out += f"{key_name}{t}{s}\n"
+            elif typ is list:
+                t = "List".ljust(15)
+                v = len(item)
+                out += f"{key_name}{t}len = {v}\n"
+    if ret: return out
+    else: print(out)
