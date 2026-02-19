c8da5b8c51a462549b2ebdefa1351570cbd1d95a
matlea <mats.leandersson@maxiv.lu.se>
2026-02-19 15:07:22 +0100

    General update. Nothing important.

--- dopey/__init__.py
+++ dopey/__init__.py
@@ -1,47 +1,46 @@
 __author__ = "Mats Leandersson"
-__version__ = "2026.02.14"
+__version__ = "2026.02.18"
 
 from colorama import Fore
 
-__all_dopey_modules__ = ["dopey_data_object", "dopey_constants", "dopey_loader", "dopey_spin", "dopey_plot", "dopey_methods", "dopey_extra"]
-__all_loaded_dopey_modules__ = []
-__missing_dopey_modules__ = []
+__all__ = ["dopey_data_object", "dopey_constants", "dopey_loader", "dopey_spin", "dopey_plot", "dopey_methods", "dopey_extra"]
 
-try: from dopey.dopey_data_object import *
-except: pass
+try: 
+    from dopey.dopey_data_object import *
+    print(f"{Fore.GREEN}dopey_data_object, version {dopey_data_object.__version__}{Fore.RESET}")
+except: print(f"{Fore.RED}dopey_data_object, not loaded{Fore.RESET}")
 
-try: from dopey.dopey_constants import *
-except: pass
+try: 
+    from dopey.dopey_constants import *
+    print(f"{Fore.GREEN}dopey_constants, version {dopey_constants.__version__}{Fore.RESET}")
+except: print(f"{Fore.RED}dopey_constants, not loaded{Fore.RESET}")
 
-try: from dopey.dopey_loader import *
-except: pass
+try: 
+    from dopey.dopey_loader import *
+    print(f"{Fore.GREEN}dopey_loader, version {dopey_loader.__version__}{Fore.RESET}")
+except: print(f"{Fore.RED}dopey_loader, not loaded{Fore.RESET}")
 
-try: from dopey.dopey_spin import *
-except: pass 
+try: 
+    from dopey.dopey_spin import *
+    print(f"{Fore.GREEN}dopey_spin, version {dopey_spin.__version__}{Fore.RESET}")
+except: print(f"{Fore.RED}dopey_spin, not loaded{Fore.RESET}")
 
-try: from dopey.dopey_plot import *
-except: pass
+try: 
+    from dopey.dopey_plot import *
+    print(f"{Fore.GREEN}dopey_plot, version {dopey_plot.__version__}{Fore.RESET}")
+except: print(f"{Fore.RED}dopey_plot, not loaded{Fore.RESET}")
 
-try: from dopey.dopey_methods import *
-except: pass
+try: 
+    from dopey.dopey_methods import *
+    print(f"{Fore.GREEN}dopey_methods, version {dopey_methods.__version__}{Fore.RESET}")
+except: print(f"{Fore.RED}dopey_methods, not loaded{Fore.RESET}")
 
-try: from dopey.dopey_extra import *
-except: pass
-
-
-gl = globals()
-for m in __all_dopey_modules__:
-    if m in gl: __all_loaded_dopey_modules__.append(m)
-    else: __missing_dopey_modules__.append(m)
-    
-if not __all_dopey_modules__ == __all_loaded_dopey_modules__:
-    print("\nNot all dopey modules were loaded.")
-    print(f"{Fore.GREEN}Loaded:     {__all_loaded_dopey_modules__}{Fore.RESET}")
-    print(f"{Fore.RED}Not loaded: {__missing_dopey_modules__}{Fore.RESET}")
-    print("This might or might not affect your usage.\n")
+try: 
+    from dopey.dopey_extra import *
+    print(f"{Fore.GREEN}dopey_extra, version {dopey_extra.__version__}{Fore.RESET}")
+except: print(f"{Fore.RED}dopey_extra, not loaded{Fore.RESET}")
 
 
 
-            
-        
-    
+
+ 
